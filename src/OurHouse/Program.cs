82f5df6f3fd6820de764d3713250fbe3d6ca7ed2
evaristocuesta <evaristocg@gmail.com>
2024-07-05 00:30:38 +0200

    Remove unused files and add logs

--- src/OurHouse/Program.cs
+++ src/OurHouse/Program.cs
@@ -9,13 +9,12 @@ builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
   new StaticResourcesInfoProvider(
     [
       new PageResource("/"),
-      new PageResource("/home/privacy"),
+      new PageResource("/home/OurHouse"),
       new CssResource("/lib/bootstrap/dist/css/bootstrap.min.css") { OptimizerType = OptimizerType.None },
       new CssResource("/css/site.css?v=pAGv4ietcJNk_EwsQZ5BN9-K4MuNYS2a9wl4Jw-q9D0"),
       new CssResource("/OurHouse.styles.css?v=QVIm3G0TQnz7jhf0QoO7Vxi4Cck3I2ZBcZUJUpvQ19o"),
       new JsResource("/lib/bootstrap/dist/js/bootstrap.bundle.min.js"),
-      new JsResource("/js/site.js?v=hRQyftXiu1lLX2P9Ly9xa4gHJgLeR1uGN5qegUobtGo"),
-      new BinResource("/favicon.png")]
+      new JsResource("/js/site.js?v=hRQyftXiu1lLX2P9Ly9xa4gHJgLeR1uGN5qegUobtGo")]
     ));
 
 var app = builder.Build();
@@ -46,8 +45,11 @@ if (args.HasExitWhenDoneArg())
 
     if (!Path.Exists(outputPath))
     {
+        Console.WriteLine($"Creating directory {outputPath}");
         Directory.CreateDirectory(outputPath);
     }
+
+    Console.WriteLine($"Generating static content in {outputPath}");
 
     app.GenerateStaticContent(outputPath,
         alwaysDefaultFile: true,
