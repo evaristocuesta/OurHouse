78aeb8273f46d59d670258f4de4af24a5204c3a0
evaristocuesta <evaristocg@gmail.com>
2024-07-11 19:48:55 +0200

    Add tests for language

--- tests/OurHouse.Tests/OurHouseTests.cs
+++ tests/OurHouse.Tests/OurHouseTests.cs
@@ -21,6 +21,23 @@ public class OurHouseTests : PageTest
         await Expect(Page).ToHaveTitleAsync(title);
     }
 
+    [Test]
+    [TestCase("", "es", "lang-es")]
+    [TestCase("en", "es", "lang-es")]
+    [TestCase("es", "en", "lang-en")]
+    [TestCase("en/ourhouse", "es/nuestracasa", "lang-es")]
+    [TestCase("es/nuestracasa", "en/ourhouse", "lang-en")]
+    public async Task ChangesToLangAsync(string origin, string target, string lang)
+    {
+        await Page.GotoAsync(origin);
+
+        var link = Page.Locator($"id={lang}");
+        await link.ClickAsync();
+
+        // Expect a title 
+        await Expect(Page).ToHaveURLAsync(target);
+    }
+
     public override BrowserNewContextOptions ContextOptions()
     {
         return new BrowserNewContextOptions()
