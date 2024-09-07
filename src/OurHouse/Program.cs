1b3af19f21116bcf88b5f4a50b45380227c9f047
evaristocuesta <evaristocg@gmail.com>
2024-09-07 19:46:06 +0200

    Change not found page design

--- src/OurHouse/Program.cs
+++ src/OurHouse/Program.cs
@@ -52,8 +52,6 @@ builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
       new PageResource($"{basePath}/en/ourhouse"),
       new PageResource($"{basePath}/es/contacto"),
       new PageResource($"{basePath}/en/contact"),
-      new PageResource($"{basePath}/es/notfound") { OutFile = $"{basePath}/es/404.html"},
-      new PageResource($"{basePath}/en/notfound") { OutFile = $"{basePath}/en/404.html"},
       new PageResource($"{basePath}/en/notfound") { OutFile = $"{basePath}/404.html"},
       new CssResource($"{basePath}/css/site.css?v=pAGv4ietcJNk_EwsQZ5BN9-K4MuNYS2a9wl4Jw-q9D0"),
       new CssResource($"{basePath}/OurHouse.styles.css?v=QVIm3G0TQnz7jhf0QoO7Vxi4Cck3I2ZBcZUJUpvQ19o"),
