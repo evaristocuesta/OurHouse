b47cb74312ae2e629c965f5e0b0a34231facc6fb
evaristocuesta <evaristocg@gmail.com>
2024-10-15 21:05:16 +0200

    wip Add gallery

--- src/OurHouse/wwwroot/js/site.js
+++ src/OurHouse/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿window.onload = (event) => {
 
-    var swiper = new Swiper(".mySwiper", {
+    var mainSwiper = new Swiper(".mainSwiper", {
         slidesPerView: "auto",
         spaceBetween: 10,
         grabCursor: true,
@@ -16,4 +16,51 @@
             enabled: true,
         }
     });
+
+    var thumbsSwiper = new Swiper(".thumbsSwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 10,
+        grabCursor: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        keyboard: {
+            enabled: true,
+        }
+    });
+
+    var slideSwiper = new Swiper(".slideSwiper", {
+        spaceBetween: 10,
+        grabCursor: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: thumbsSwiper
+        }
+    });
+
+    const photos = document.querySelectorAll(".mainSwiper .swiper-slide");
+
+    if (photos) {
+        console.log(photos);
+
+        photos.forEach((photo) => {
+            photo.onclick = (event) => {
+                console.log("se ejecuta el evento");
+
+                const myModal = new bootstrap.Modal('#modalGallery', {
+                    keyboard: false
+                });
+
+                myModal.show();
+            };
+        });
+    }
 };
